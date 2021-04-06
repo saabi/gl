2c1653c0ae8a96e76ebdff766b1d9be1cf428330
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2021-04-06 15:36:01 -0300

    Updated XRCamera

--- scene/cameras/XRCamera.svelte
+++ scene/cameras/XRCamera.svelte
@@ -1,16 +1,14 @@
 <script>
-	import { writable } from 'svelte/store';
 	import { get_scene, get_parent } from '../../internal/index.mjs';
 	import * as mat4 from 'gl-matrix/mat4';
 
 	export let location = [0, 0, 0];
-	export let lookAt = null;
 	export let up = [0, 1, 0];
 	export let fov = 60;
 	export let near = 1;
 	export let far = 20000;
 
-	const { add_camera, update_camera, width, height, get_target } = get_scene();
+	const { add_xrCamera, update_camera, width, height } = get_scene();
 	const { ctm } = get_parent();
 
 	const matrix = mat4.create();
@@ -20,25 +18,13 @@
 	let camera = {
 		matrix,
 		world_position,
-		view: mat4.create(),
 		projection: mat4.create()
 	};
 
-	let target = writable(null);
-
-	$: if (typeof lookAt === 'string') {
-		target = get_target(lookAt);
-	} else {
-		target.set(lookAt);
-	}
-
 	$: camera.matrix = (
 		mat4.translate(camera.matrix, $ctm, location),
-		$target && mat4.targetTo(camera.matrix, world_position, $target, up),
 		camera.matrix
 	);
-
-	$: camera.view = mat4.invert(camera.view, camera.matrix);
 
 	$: camera.projection = mat4.perspective(
 		camera.projection,
@@ -48,7 +34,11 @@
 		far
 	);
 
-	$: update_camera(camera);
+	$: {
+		camera.world_position[0] = location[0];
+		camera.world_position[1] = location[1];
+		camera.world_position[2] = location[2];
+	};
 
-	add_camera(camera);
+	add_xrCamera(camera);
 </script>
